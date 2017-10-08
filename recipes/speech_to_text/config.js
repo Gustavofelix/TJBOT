56086d8c505abb847a76f1dbd83a79807d28eb82
Gustavofelix <32615577+Gustavofelix@users.noreply.github.com>
2017-10-08 18:50:12 +0000

    Update config.js

--- recipes/speech_to_text/config.js
+++ recipes/speech_to_text/config.js
@@ -1,4 +1,4 @@
-exports.password = 'v7VKpBUjkmKz' ;
-exports.username = '79d6fc92-ed8a-454f-b9ac-dc24677c3878' ;
-exports.version = 'v1' ;
+  "url": "https://stream.watsonplatform.net/speech-to-text/api",
+  "username": "79d6fc92-ed8a-454f-b9ac-dc24677c3878",
+  "password": "v7VKpBUjkmKz"
 
