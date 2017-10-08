4ab2dc05fc3c42146eb767af76d4fd569f05d47a
Gustavofelix <32615577+Gustavofelix@users.noreply.github.com>
2017-10-08 17:07:13 +0000

    Update config.js

--- recipes/speech_to_text/config.js
+++ recipes/speech_to_text/config.js
@@ -1,6 +1,6 @@
 // Please replace the username and password with your bluemix credentials
 
 
-exports.password = 'xxxxxx' ;
-exports.username = 'xxx-xxx-xxx' ;
+exports.password = '79d6fc92-ed8a-454f-b9ac-dc24677c3878' ;
+exports.username = 'v7VKpBUjkmKz' ;
 exports.version = 'v1' ;
