90763ab744356604f4964be779b92a8c1a71c3ee
Gustavofelix <32615577+Gustavofelix@users.noreply.github.com>
2017-10-08 22:56:39 +0000

    Update config.js

--- recipes/speech_to_text/config.js
+++ recipes/speech_to_text/config.js
@@ -1,3 +1,3 @@
-exports.password = 'xxxxxx' ;
-exports.username = 'xxx-xxx-xxx' ;
+exports.password = 'v7VKpBUjkmKz' ;
+exports.username = '79d6fc92-ed8a-454f-b9ac-dc24677c3878' ;
 exports.version = 'v1' ;
