d178509f1f9e9c360973567d95a6c14458ce2894
Gustavofelix <32615577+Gustavofelix@users.noreply.github.com>
2017-10-08 18:22:54 +0000

    Update config.js

--- recipes/speech_to_text/config.js
+++ recipes/speech_to_text/config.js
@@ -1,18 +1,4 @@
+exports.password = 'v7VKpBUjkmKz' ;
+exports.username = '79d6fc92-ed8a-454f-b9ac-dc24677c3878' ;
+exports.version = 'v1' ;
 
-/*
-User-specific configuration
-    ** IMPORTANT NOTE ********************
-    * Please ensure you do not interchange your username and password.
-    * Hint: Your username is the lengthy value ~ 36 digits including a hyphen
-    * Hint: Your password is the smaller value ~ 12 characters
-*/ 
-
-// Create the credentials object for export
-exports.credentials = {};
-
-// Watson Speech to Text
-// https://www.ibm.com/watson/developercloud/speech-to-text.html
-exports.credentials.speech_to_text = {
-    password: 'v7VKpBUjkmKz',
-    username: '79d6fc92-ed8a-454f-b9ac-dc24677c3878'
-};
